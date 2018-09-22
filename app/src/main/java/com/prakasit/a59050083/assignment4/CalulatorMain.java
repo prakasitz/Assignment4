8a24e64e469d130d1323e041ce6ab88af75d9997
prakasit <prakasitbm@gmail.com>
2018-09-22 13:36:21 +0700

    Asssigment5 เพิ่มปุ่มบนเมนูบา กดแล้วเปลี่ยนธีม แต่ ที่แสดง input หายไป

--- app/src/main/java/com/prakasit/a59050083/assignment4/CalulatorMain.java
+++ app/src/main/java/com/prakasit/a59050083/assignment4/CalulatorMain.java
@@ -3,6 +3,8 @@ package com.prakasit.a59050083.assignment4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,12 +19,42 @@ public class CalulatorMain extends AppCompatActivity implements
             btnClear, btnDel, btnPlus,
             btnMinus, btnMutiple, btnDevide;
     TextView textMain, textSub;
+    int cnt = 1;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        cnt++;
+        int idOfItem = item.getItemId();
+        switch (idOfItem){
+            case R.id.menuItem1: {
+                Toast.makeText(this, "Item1 clicked:" + cnt,
+                        Toast.LENGTH_LONG).show();
+                    if(cnt%2 == 0) {
+                        setContentView(R.layout.activity_calulator_main_1);
+                        a();
+                    } else {
+                        setContentView(R.layout.activity_calulator_main);
+                        a();
+                    }
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calulator_main);
+        a();
+    }
 
+    public void a() {
         btnClear = findViewById(R.id.buttonClear); //Clear
         btnDel = findViewById(R.id.buttonDel); //Delete
         btnPlus = findViewById(R.id.buttonPlus); // +
@@ -68,7 +100,6 @@ public class CalulatorMain extends AppCompatActivity implements
 
         textMain = findViewById(R.id.MainText);
         textSub = findViewById(R.id.SubText);
-
     }
 
     private String str = "";
